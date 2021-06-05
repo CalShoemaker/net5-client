65a310f380f6128520d8c6c4ae94783643830263
Cal Shoemaker <cal.shoemaker@gmail.com>
2021-06-05 16:22:32 -0700

    Add router & some stub components

---
+++ src/components/news/index.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="news">
+    <h2>News</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'News'
+}
+</script>
---
+++ src/views/Home.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="home">
+    <News />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import News from '@/components/news/index.vue'
+
+export default {
+  name: 'Home',
+  components: {
+    News
+  }
+}
+</script>
